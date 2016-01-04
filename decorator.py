48c6c7d679e8ac1e66e3f2dff64f79c8293fb1e6
soaringEveryday <790445149@qq.com>
2016-01-04 13:58:38 +0800

    decorator

--- decorator.py
+++ decorator.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 # 编写无参数的decorator，可以代替f = log()这样的临时变量f
+import time
+
 
 # 这里的log只接受一个参数，因为fn只含有一个参数，所以如果factorial是多个参数，会报错
 def log(f):
@@ -33,3 +35,20 @@ def factorial(n, x):
 
 
 print factorial(10, 2)
+
+
+def performance(f):
+    def fn(*args, **kw):
+        start = time.time()
+        r = f(*args, **kw)
+        end = time.time()
+        print 'call %s() in %fs' % (f.__name__, (end - start))
+        return r
+    return fn
+
+
+@performance
+def factorial(n):
+    return reduce(lambda x,y: x*y, range(1, n+1))
+
+print factorial(10)
